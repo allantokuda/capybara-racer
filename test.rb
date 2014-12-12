b7f7491e660b24abff63f084c7f4a2387439e43e
Allan Tokuda <allan.tokuda@code42.com>
2014-12-12 17:42:36 -0600

    A little extra wait time and comments

--- test.rb
+++ test.rb
@@ -26,17 +26,17 @@ describe 'Capybara' do
     expect(find('#test-subject')).to have_content 'Hello, world!'
     end_time = Time.now
 
-    expect(end_time - start_time).to be < 0.5 # seconds
+    expect(end_time - start_time).to be < 0.6 # seconds
   end
 
   it 'only waits up to a timeout (default of 2 seconds) for changes' do
-    fill_in 'Delay', with: 2500
+    fill_in 'Delay', with: 2500 # JavaScript setTimeout() milliseconds
     click_link 'Change'
     expect(find('#test-subject')).to_not have_content 'Hello, world!'
   end
 
   it 'allows wait time to be overridden' do
-    fill_in 'Delay', with: 2500 # JavaScript setTimeout() milliseconds
+    fill_in 'Delay', with: 2500
     click_link 'Change'
     using_wait_time(3) do # Capybara seconds
       expect(find('#test-subject')).to have_content 'Hello, world!'
@@ -45,9 +45,11 @@ describe 'Capybara' do
 
   it 'can help you even when you are not working with it directly' do
     # Actually, it can't (reliably) help you, which is why this test fails.
+
+    # The race starts here...
     click_link 'Change'
 
-    # Play with this sleep value and you can make the test "pass".
+    # You can play with this sleep value to slow down the test and make it "pass".
     # But this is NOT recommended. Much better to let Capybara poll the site for you.
     sleep(0.5)
 
