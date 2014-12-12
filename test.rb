ce3640c07d2f123749ee098cf3696113139b5468
Allan Tokuda <allan.tokuda@code42.com>
2014-12-12 17:31:48 -0600

    More examples and detail

--- test.rb
+++ test.rb
@@ -15,17 +15,47 @@ describe 'Capybara' do
 
   it 'waits for the page to change' do
     click_link 'Change'
-    expect(find('#test-subject')).to have_text 'Hello, world!'
+    expect(find('#test-subject')).to have_content 'Hello, world!'
+  end
+
+  it 'does not wait any longer than it needs to' do
+    fill_in 'Delay', with: 500
+    click_link 'Change'
+
+    start_time = Time.now
+    expect(find('#test-subject')).to have_content 'Hello, world!'
+    end_time = Time.now
+
+    expect(end_time - start_time).to be < 0.5 # seconds
+  end
+
+  it 'only waits up to a timeout (default of 2 seconds) for changes' do
+    fill_in 'Delay', with: 2500
+    click_link 'Change'
+    expect(find('#test-subject')).to_not have_content 'Hello, world!'
   end
 
   it 'allows wait time to be overridden' do
-    using_wait_time(200) do
-      # expect that the field does not change during the first 200 milliseconds.
-      expect(find('#test-subject').text).to_not eq 'Hello, world!'
+    fill_in 'Delay', with: 2500 # JavaScript setTimeout() milliseconds
+    click_link 'Change'
+    using_wait_time(3) do # Capybara seconds
+      expect(find('#test-subject')).to have_content 'Hello, world!'
     end
   end
 
   it 'can help you even when you are not working with it directly' do
+    # Actually, it can't (reliably) help you, which is why this test fails.
+    click_link 'Change'
+
+    # Play with this sleep value and you can make the test "pass".
+    # But this is NOT recommended. Much better to let Capybara poll the site for you.
+    sleep(0.5)
+
+    # Here we are calling `text` on what Capybara found.
+    # The result of that is a string, not a Capybara object. All Capybara can
+    # do is hand over the value once it's found, and assume you know what you're doing.
+    # Then when we immediately check its value, we're asserting our change
+    # condition before the page has updated, so the test fails.
     expect(find('#test-subject').text).to eq 'Hello, world!'
   end
 end
